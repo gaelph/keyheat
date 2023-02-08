3abb53a7eccd2d483a20ee3385263936f0fd2abb
Gaël Philipe <gaelph@gmail.com>
2023-02-08 09:46:28 +0100

    add support for layers

--- src/App.js
+++ src/App.js
@@ -3,253 +3,118 @@ import "./App.css";
 import * as Heatmap from "heatmap.js";
 
 import data from "./log.json";
+import Layers from "./layers";
 
 function formatData(matrix) {
-	let entries = [];
-	let max = 0;
+  let entries = [];
+  let max = 0;
 
-	Object.entries(matrix).forEach(([r, row]) => {
-		Object.entries(row).forEach(([c, col]) => {
-			const entry = {
-				x: (c + 1) * 6 + 40,
-				y: (r + 1) * 6 + 40,
-				value: col,
-			};
-			if (entry.value > max) max = entry.value;
+  Object.entries(matrix).forEach(([r, row]) => {
+    Object.entries(row).forEach(([c, col]) => {
+      const entry = {
+        x: (c + 1) * 6 + 40,
+        y: (r + 1) * 6 + 40,
+        value: col,
+      };
+      if (entry.value > max) max = entry.value;
 
-			entries.push(entry);
-		});
-	});
+      entries.push(entry);
+    });
+  });
 
-	entries = entries.map((e) => {
-		e.value = e.value > 0 ? e.value / max : 0.001;
-		e.value = 1 + Math.log(e.value / 4 + 0.01);
-		return e;
-	});
-	max = entries.map((e) => e.value).reduce((m, e) => (e > m ? e : m), 0);
-	let min = entries.map((e) => e.value).reduce((m, e) => (e < m ? e : m), 0);
-	// max = 1;
-	// console.log(entries);
+  entries = entries.map((e) => {
+    e.value = e.value > 0 ? e.value / max : 0.001;
+    e.value = 1 + Math.log(e.value / 4 + 0.01);
+    return e;
+  });
+  max = entries.map((e) => e.value).reduce((m, e) => (e > m ? e : m), 0);
+  let min = entries.map((e) => e.value).reduce((m, e) => (e < m ? e : m), 0);
+  // max = 1;
+  // console.log(entries);
 
-	return {
-		max,
-		min,
-		data: entries,
-	};
+  return {
+    max,
+    min,
+    data: entries,
+  };
 }
 
 const KEY_WIDTH = 59;
 
-const keyStyle = {
-	width: KEY_WIDTH,
-	height: KEY_WIDTH,
-	border: "1px solid black",
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
+const rowStyle = {
+  height: KEY_WIDTH,
+  display: "flex",
+  flexDirection: "row",
+  width: "1200",
 };
 
-const rowStyle = {
-	height: KEY_WIDTH,
-	display: "flex",
-	flexDirection: "row",
-	width: "1200",
-};
+function HeatmapComponent({ layerId, matrix }) {
+  const canvasRef = useRef();
+  const heatmap = useRef();
+  useEffect(() => {
+    if (canvasRef.current) {
+      for (let child of canvasRef.current.children) {
+        if (child.tagName.toLowerCase() === "canvas") {
+          child.remove();
+        }
+      }
+      heatmap.current = Heatmap.create({
+        container: canvasRef.current,
+        radius: KEY_WIDTH,
+      });
+
+      heatmap.current.setData(formatData(matrix));
+    }
+  }, [matrix]);
+
+  return (
+    <>
+      <h3>Layer #{layerId}</h3>
+      <div
+        id="heatmap"
+        style={{ width: "1200px", height: "400px" }}
+        ref={canvasRef}
+      >
+        <div
+          style={{
+            position: "absolute",
+            zIndex: 10,
+            top: 17,
+            left: 17,
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          {Layers[layerId].map((layerRow) => (
+            <div style={rowStyle}>
+              {layerRow.map((char) => (
+                <div className="key">
+                  <span>{char}</span>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
 
 function App() {
-	const heatmapElmtRef = useRef();
-	const heatmap = useRef();
-
-	useEffect(() => {
-		if (heatmapElmtRef.current) {
-			for (let child of heatmapElmtRef.current.children) {
-				if (child.tagName.toLowerCase() === "canvas") {
-					child.remove();
-				}
-			}
-			heatmap.current = Heatmap.create({
-				container: heatmapElmtRef.current,
-				radius: KEY_WIDTH,
-			});
-
-			heatmap.current.setData(formatData(data.matrix));
-		}
-	}, []);
-	return (
-		<div className="App">
-			<div
-				id="heatmap"
-				style={{ width: "1200px", height: "400px" }}
-				ref={heatmapElmtRef}
-			>
-				<div
-					style={{
-						position: "absolute",
-						zIndex: 10,
-						top: 17,
-						left: 17,
-						width: "100%",
-						height: "100%",
-					}}
-				>
-					<div style={rowStyle}>
-						<div className="key">⇥</div>
-						<div className="key">
-							<span>Q</span>
-							<span>&</span>
-						</div>
-						<div className="key">
-							<span>C</span>
-							<span>{"<"}</span>
-						</div>
-						<div className="key">
-							<span>L</span>
-							<span>[</span>
-						</div>
-						<div className="key">
-							<span>P</span>
-							<span>]</span>
-						</div>
-						<div className="key">
-							<span>B</span>
-							<span>/</span>
-						</div>
-						<div className="key">
-							<span>K</span>
-						</div>
-						<div className="key">
-							<span>F</span>
-							<span>"</span>
-						</div>
-						<div className="key">
-							<span>O</span>
-							<span>'</span>
-						</div>
-						<div className="key">
-							<span>Y</span>
-						</div>
-						<div className="key">
-							<span>-</span>
-							<span>_</span>
-						</div>
-						<div className="key">
-							<span>←</span>
-						</div>
-					</div>
-					<div style={rowStyle}>
-						<div className="key">
-							<span>␛</span>
-						</div>
-						<div className="key">
-							<span>A</span>
-							<span>`</span>
-						</div>
-						<div className="key">
-							<span>S</span>
-							<span>=</span>
-						</div>
-						<div className="key">
-							<span>R</span>
-							<span>(</span>
-						</div>
-						<div className="key">
-							<span>T</span>
-							<span>)</span>
-						</div>
-						<div className="key">
-							<span>G</span>
-							<span>!</span>
-						</div>
-						<div className="key">
-							<span>M</span>
-							<span>^</span>
-						</div>
-						<div className="key">
-							<span>N</span>
-							<span>:</span>
-						</div>
-						<div className="key">
-							<span>E</span>
-							<span>;</span>
-						</div>
-						<div className="key">
-							<span>I</span>
-						</div>
-						<div className="key">
-							<span>U</span>
-							<span>@</span>
-						</div>
-						<div className="key">
-							<span>r</span>
-							<span>$</span>
-						</div>
-					</div>
-					<div style={rowStyle}>
-						<div className="key">
-							<span></span>
-						</div>
-						<div className="key">
-							<span>Z</span>
-							<span>|</span>
-						</div>
-						<div className="key">
-							<span>V</span>
-							<span>{">"}</span>
-						</div>
-						<div className="key">
-							<span>J</span>
-							<span>{"{"}</span>
-						</div>
-						<div className="key">
-							<span>D</span>
-							<span>{"}"}</span>
-						</div>
-						<div className="key">
-							<span>W</span>
-							<span>%</span>
-						</div>
-						<div className="key">
-							<span>X</span>
-							<span>+</span>
-						</div>
-						<div className="key">
-							<span>H</span>
-							<span>-</span>
-						</div>
-						<div className="key">
-							<span>,</span>
-							<span>#</span>
-						</div>
-						<div className="key">
-							<span>.</span>
-							<span>…</span>
-						</div>
-						<div className="key">
-							<span>/</span>
-							<span>\</span>
-						</div>
-						<div className="key">
-							<span></span>
-						</div>
-					</div>
-					<div style={rowStyle}>
-						<div className="key"></div>
-						<div className="key"></div>
-						<div className="key"></div>
-						<div className="key"></div>
-						<div className="key">⇧</div>
-						<div className="key">⎵</div>
-						<div className="key">↲</div>
-						<div className="key">⇧</div>
-						<div className="key"></div>
-						<div className="key"></div>
-						<div className="key"></div>
-						<div className="key"></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <ul>
+        {data.layers.map((matrix, layerId) => {
+          if (matrix === null || matrix === undefined) return null;
+          return (
+            <li key={layerId}>
+              <HeatmapComponent matrix={matrix} layerId={layerId} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
